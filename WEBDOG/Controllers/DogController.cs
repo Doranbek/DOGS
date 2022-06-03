9fd49ce1f3dc34640d7f227dfca928e6630506d4
Doranbek <ddr3lenta@gmail.com>
2022-06-03 10:48:52 +0600

    Create DogController

---
+++ WEBDOG/Controllers/DogController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using WEBDOG.Data;
+
+namespace WEBDOG.Controllers
+{
+    public class DogController : Controller
+    {
+        private readonly ILogger<DogController> _logger;
+        private readonly AppDbContext db;
+
+        public DogController(ILogger<DogController> logger, AppDbContext db)
+        {
+            _logger = logger;
+            this.db = db;
+        }
+        // GET: DogController
+        public async Task<ActionResult> Index()
+        {
+            var listModel = await db.Dogs.ToListAsync();
+            return View(listModel);
+        }
+
+        // GET: DogController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DogController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DogController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DogController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DogController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DogController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DogController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- WEBDOG/Models/DogModel.cs
+++ WEBDOG/Models/DogModel.cs
@@ -1,66 +1,64 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WEBDOG.Data
+namespace WEBDOG.Models
 {
-    public class Dog
+    public class DogModel
     {
         [Key]
-        [Required]
         public Guid id { get; set; }
 
         [Display(Name = "COATO")]
+        [Required(ErrorMessage = "Не указан COATO")]
         public int CoatoId { get; set; }
-
-
-        [Required]
         [Display(Name = "Организация")]
+        [Required(ErrorMessage = "Не указан организация")]
         public int OrganizationId { get; set; }
 
-        [Required]
         [Display(Name = "Регистрационный номер")]
         public string TagNumber { get; set; }
-
         [Display(Name = "Дата создания")]
-        public DateTime CreatedDate { get; set; }
-
-        [Required]
+        public DateTime? CreatedDate { get; set; }
         [Display(Name = "Ф.И.О. владелца")]
+        [Required(ErrorMessage = "Не указан владелец")]
         public string Owner { get; set; }
 
-        [Display(Name = "Tелефон")]
+        [Display(Name = "Номер телефона")]
         public string PhoneNumber { get; set; }
 
         [Display(Name = "Адрес")]
-        [Required]
+        [Required(ErrorMessage = "Не указан адрес")]
         public string Address { get; set; }
-
-        [Required]
         [Display(Name = "Имя собак")]
+        [Required(ErrorMessage = "Не заполнен имя собак")]
         public string DogName { get; set; }
 
-
         [Display(Name = "Цвет собак")]
+        [Required(ErrorMessage = "Не заполнен цвет")]
         public string Colour { get; set; }
 
-
         [Display(Name = "Пол")]
+        [Required(ErrorMessage = "Не заполнен пол собак")]
         public int Gender { get; set; }
 
-
         [Display(Name = "Год рождение")]
+        [Required(ErrorMessage = "Не заполнен год рождение собак")]
         public int BirthYear { get; set; }
 
-
         [Display(Name = "Порода")]
+        [Required(ErrorMessage = "Не заполнен порода собак")]
         public string Breed { get; set; }
-
-
         [Display(Name = "Описание")]
         public string Description { get; set; }
 
-
         [Display(Name = "Состояние")]
         public int IsAlive { get; set; }
+
+        //public IEnumerable<SelectListItem> Category { get; set; }        
+        //public IEnumerable<SelectListItem> Organization { get; set; }
+        //public IEnumerable<SelectListItem> Coato { get; set; }
     }
 }
