06354e8708b4919b1c5024891d3d3b5c71a68245
Doranbek <ddr3lenta@gmail.com>
2022-06-06 13:07:28 +0600

    Create class DrugModel

--- WEBDOG/Models/DrugModel.cs
+++ WEBDOG/Models/DrugModel.cs
@@ -1,28 +1,24 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace WEBDOG.Data
+namespace WEBDOG.Models
 {
-    public class Drug
+    public class DrugModel
     {
         [Key]
-        [Required]
         public int Id { get; set; }
-
-
         [Display(Name = "Название")]
+        [Required(ErrorMessage = "Не указан названия")]
         public string Name { get; set; }
-
-
         [Display(Name = "серийный номер")]
+        [Required(ErrorMessage = "Не указан серийный номер")]
         public string SerialNumber { get; set; }
-
-
+        
         [Display(Name = "срок годности")]
+        [Required(ErrorMessage = "Не указан срок годност")]
         public DateTime ExpirationDate { get; set; }
-
+        
         [Display(Name = "Описание")]
-        public string Description { get; set; }        
-
+        public string Description { get; set; }
     }
 }
