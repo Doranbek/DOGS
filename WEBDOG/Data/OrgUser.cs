66ae4edf3ae1c5581d7bf34989fcd794416a7af3
Doranbek <ddr3lenta@gmail.com>
2022-06-09 17:42:31 +0600

    fix OrgUser

--- WEBDOG/Data/OrgUser.cs
+++ WEBDOG/Data/OrgUser.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WEBDOG.Data
 {
@@ -7,6 +8,6 @@ namespace WEBDOG.Data
         [Key]
         public int Id { get; set; }
         public int OrganizationId { get; set; }
-        public string UserId { get; set; }
+        public  string UserName { get; set; }
     }
 }
