cd56110b74cddf3d9f9cd4885d804222c6672add
Doron Doron <doronkg@yandex.ru>
2022-06-27 01:04:04 +0600

    add views to report

---
+++ WEBDOG/Models/DogDaaryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using WEBDOG.Data;
+
+namespace WEBDOG.Models
+{
+    public class DogDaaryViewModel
+    {
+        public List<DogDaary> DogKaroos { get; set; }
+        public Guid DogId { get; set; }
+    }
+}
---
+++ WEBDOG/Models/DogKarooViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using WEBDOG.Data;
+
+namespace WEBDOG.Models
+{
+    public class DogKarooViewModel
+    {
+        public List<DogKaroo> DogKaroos { get; set; }
+        public Guid DogId { get; set; }
+    }
+}
