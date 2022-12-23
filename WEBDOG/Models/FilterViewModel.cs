6091e455f4dc7faa51a5147cc0bc235e16a0909d
KKenjebek <106551700+KKenjebek@users.noreply.github.com>
2022-12-23 15:42:40 +0600

    fix23122022_2

--- WEBDOG/Models/FilterViewModel.cs
+++ WEBDOG/Models/FilterViewModel.cs
@@ -5,16 +5,16 @@ namespace WEBDOG.Models
 {
     public class FilterViewModel
     {
-        //public FilterViewModel(List<selectCoats> companies, int? company, string name)
-        //{
-        //    // устанавливаем начальный элемент, который позволит выбрать всех
-        //    companies.Insert(0, new Company { Name = "Все", Id = 0 });
-        //    Companies = new SelectList(companies, "Id", "Name", company);
-        //    SelectedCompany = company;
-        //    SelectedName = name;
-        //}
-        //public SelectList Companies { get; private set; } // список компаний
-        //public int? SelectedCompany { get; private set; }   // выбранная компания
-        //public string SelectedName { get; private set; }    // введенное имя
+        ////public FilterViewModel(List<selectCoats> companies, int? company, string name)
+        ////{
+        ////    // устанавливаем начальный элемент, который позволит выбрать всех
+        ////    companies.Insert(0, new Company { Name = "Все", Id = 0 });
+        ////    Companies = new SelectList(companies, "Id", "Name", company);
+        ////    SelectedCompany = company;
+        ////    SelectedName = name;
+        ////}
+        ////public SelectList Companies { get; private set; } // список компаний
+        ////public int? SelectedCompany { get; private set; }   // выбранная компания
+        ////public string SelectedName { get; private set; }    // введенное имя
     }
 }
