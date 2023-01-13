79dd9ad859167ff7c8fed5995a7ec9a6d3f762a6
KKenjebek <106551700+KKenjebek@users.noreply.github.com>
2023-01-13 17:41:18 +0600

    fix130123

--- WEBDOG/Models/FilterViewModel.cs
+++ WEBDOG/Models/FilterViewModel.cs
@@ -1,20 +1,22 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using WEBDOG.Data;
 
 namespace WEBDOG.Models
 {
     public class FilterViewModel
     {
-        ////public FilterViewModel(List<selectCoats> companies, int? company, string name)
-        ////{
-        ////    // устанавливаем начальный элемент, который позволит выбрать всех
-        ////    companies.Insert(0, new Company { Name = "Все", Id = 0 });
-        ////    Companies = new SelectList(companies, "Id", "Name", company);
-        ////    SelectedCompany = company;
-        ////    SelectedName = name;
-        ////}
-        ////public SelectList Companies { get; private set; } // список компаний
-        ////public int? SelectedCompany { get; private set; }   // выбранная компания
-        ////public string SelectedName { get; private set; }    // введенное имя
+        public FilterViewModel(List<Coato> coatos, int? coatsin)
+        {
+            // устанавливаем начальный элемент, который позволит выбрать всех
+            coatos.Insert(0, new Coato { Name = "Все", Id = 0 });
+            Companies = new SelectList(coatos, "Id", "Name", coatsin);
+            SelectedCompany = coatsin;
+            
+        }
+        public SelectList Companies { get; private set; } // список компаний
+        public int? SelectedCompany { get; private set; }   // выбранная компания
+       
     }
 }
--- WEBDOG/Models/IndexViewModel.cs
+++ WEBDOG/Models/IndexViewModel.cs
@@ -7,8 +7,8 @@ namespace WEBDOG.Models
         public class IndexViewModel
         {
             public IEnumerable<ViewDog> ViewDogs { get; set; }
-            //public IEnumerable<View_KarooDaary> View_KarooDaarys { get; set; }
             public PageViewModel PageViewModel { get; set; }
-        }
+            public FilterViewModel FilterViewModel { get; set; }
+    }
     
 }
